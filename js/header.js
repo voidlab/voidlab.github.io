150dd8dced9f24eb2501cf6a50e8c1d5daeb1ea7
kate hollenbach <kjhollen@gmail.com>
2016-08-24 17:19:35 -0700

    wip / temp commit: animate in menu

--- js/header.js
+++ js/header.js
@@ -6,4 +6,18 @@ $(document).ready(function () {
             $("header").removeClass("scrolled");
         }
     });
+
+	var nav = $('.menu-icon');
+	console.log(nav);
+    nav.bind('click', function () {
+    	var navHeight = $('.site-nav .trigger-wrapper').height();
+    	var newNavHeight = $('.site-nav .trigger').height();
+    	console.log("nav height: " + navHeight + " newNavHeight" + newNavHeight);
+
+    	if (navHeight == 0) {
+    		$('.site-nav .trigger-wrapper').animate({'height':newNavHeight+'px'}, 500);
+    	} else {
+    		$('.site-nav .trigger-wrapper').animate({'height':'0px'}, 500);
+    	}
+    });
 });
